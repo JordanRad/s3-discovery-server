00b9b0d7ceb6b1fde6e51fd0de4deccd67031d9a
= <=>
2020-09-29 10:52:45 +0200

    yml init

--- src/main/java/com/example/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/com/example/discoveryserver/DiscoveryServerApplication.java
@@ -2,11 +2,15 @@ package com.example.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
